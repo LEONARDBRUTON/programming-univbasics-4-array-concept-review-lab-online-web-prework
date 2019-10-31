095c22482edbc8b860066d08047427cfaf0a977b
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-31 19:25:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,33 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
-end
 
-def find_max_value(array)
-  # Add your solution here
+
+
+def find_element_index(array, value_to_find)
+  array.length.times do |index|
+    if array[index] == value_to_find
+      return index 
+    end
+  end
+  nil
 end
+def find_max_value(array)
+  max = 0
+    array.length.times do |index|
+   if array[index] > max
+     max = array[index]
+    end
+  end
+  return max
+end
+  
+
 
 def find_min_value(array)
-  # Add your solution here
+   min = 10
+    array.length.times do |index|
+   if array[index] < min
+     min = array[index]
+    end
+  end
+  return min
+  
 end
